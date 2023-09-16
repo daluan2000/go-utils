c7bb0853e0a2ea4b7f580ff223c9d6cc039e639e
luan <2432290245@qq.com>
2023-09-16 10:08:23 +0800

    feat:配日志文件管理工具类

---
+++ config_manager/config_manager_interface/config_manager_interface.go
@@ -0,0 +1,30 @@
+package config_manager_interface
+
+import (
+	"github.com/fsnotify/fsnotify"
+	"time"
+)
+
+// CreateConfigManager 创建ConfigManager对象的工厂函数，
+// configType是配置文件类型，可选json，yaml，下层用的是 github.com/spf13/viper包，更多支持的文件类型可去它文档查看
+// configPath是配置文件所在目录，configName是配置文件名（不包含后缀），程序会自动扫描configPath包括的所有目录
+// 但程序只会添加一个文件并读取文件里的配置，所以一个 ConfigManager 对象只能读取一个配置文件
+type CreateConfigManager func(configType string, configPath []string, configName string) (ConfigManagerInterface, error)
+
+// ConfigManagerInterface 是ConfigManager的借口，定义了所有会用到的方法
+type ConfigManagerInterface interface {
+
+	// Get GetString GetBool ...... 根据key获取值
+	Get(key string) interface{}
+	GetString(key string) string
+	GetBool(key string) bool
+	GetInt(key string) int
+	GetInt32(key string) int32
+	GetInt64(key string) int64
+	GetFloat64(key string) float64
+	GetDuration(key string) time.Duration
+	GetStringSlice(key string) []string
+
+	// AddConfigWatcher callback在文件发生变化时会被调用
+	AddConfigWatcher(callback func(changeEvent fsnotify.Event))
+}
---
+++ examples/config_manager/config_manager_test.go
@@ -0,0 +1,35 @@
+package config_manager
+
+import (
+	"fmt"
+	"go-utils/config_manager"
+	"testing"
+)
+
+// 程序运行时的当前目录，等于程序启动文件所在的目录
+func Test_Example(t *testing.T) {
+	cm, _ := config_manager.CreateConfigManager("yaml", []string{"./config"}, "yaml_config")
+	fmt.Println(cm.Get("Mysql"))
+	fmt.Println(cm.GetInt("Mysql.port"))
+	fmt.Println(cm.GetString("Mysql.UserName"))
+	fmt.Println(cm.GetBool("Mysql.Writable"))
+	fmt.Println(cm.GetDuration("Mysql.Timeout").Minutes())
+	// 输出内容：
+	// map[port:3106 username:root writable:false]
+	// 3106
+	// root
+	// false
+	// 30
+
+	cm, _ = config_manager.CreateConfigManager("json", []string{"./config"}, "json_config")
+	fmt.Println(cm.Get("student"))
+	fmt.Println(cm.GetInt("student.age"))
+	fmt.Println(cm.GetString("student.name"))
+	fmt.Println(cm.GetStringSlice("schools"))
+	// 输出内容：
+	// map[age:22 name:luan]
+	// 22
+	// luan
+	// [collageA collageB]
+
+}
